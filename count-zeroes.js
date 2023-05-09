95c53f936a9a243e657f706d785f5caa99524523
alocke8181 <alocke_8181@yahoo.com>
2023-05-09 17:38:40 -0400

    initalized

---
+++ count-zeroes.js
@@ -0,0 +1,17 @@
+function countZeroes(arr) {
+  let floor = 0;
+  let ceiling = arr.length-1;
+  let index;
+  while(floor <= ceiling){
+    index = Math.floor((floor+ceiling)/2);
+    val = arr[index];
+    if(val===1){
+      floor = index + 1;
+    }
+    else{
+      ceiling = index -1;
+    }
+  }
+}
+
+module.exports = countZeroes
