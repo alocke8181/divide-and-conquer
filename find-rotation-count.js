771ada3eac94a32a6862e2f6272276e768181725
alocke8181 <alocke_8181@yahoo.com>
2023-05-09 18:36:14 -0400

    completed rotation count

--- find-rotation-count.js
+++ find-rotation-count.js
@@ -1,5 +1,24 @@
-function findRotationCount() {
-  
+function findRotationCount(arr, floor = 0, ceiling = arr.length-1) {
+    if (ceiling < floor){
+        return 0;
+    }
+    if (ceiling===floor){
+        return floor;
+    }
+
+    index = Math.floor((floor+ceiling)/2);
+
+    if(index < ceiling && arr[index+1] < arr[index]){
+        return index+1;
+    }
+    if(index > floor && arr[index] < arr[index-1]){
+        return index;
+    }
+    if(arr[ceiling] > arr[index]){
+        return findRotationCount(arr, floor, index-1);
+    }
+    return findRotationCount(arr, index+1, ceiling);
+
 }
 
 //module.exports = findRotationCount
