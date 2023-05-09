5d6c67f1cad0eb9b11e8b684c676ee5940f335b1
alocke8181 <alocke_8181@yahoo.com>
2023-05-09 18:07:52 -0400

    completed find_floor

--- find-floor.js
+++ find-floor.js
@@ -1,5 +1,25 @@
-function findFloor() {
+function findFloor(arr, val, floor = 0, ceiling = arr.length -1){
+    console.log("findfloor");
+    if(floor > ceiling){
+        return -1;
+    }
+    if (arr[ceiling] <= val){
+        return arr[ceiling];
+    }
+    let index = Math.floor((floor+ceiling)/2);
+    let midVal = arr[index];
+    if(midVal == val){
+        return midVal;
+    }
+    if(index > 0 && arr[index-1] <= val && val < midVal){
+        return arr[index -1];
+    }
+    if(val < midVal){
+        return findFloor(arr, val, floor, index-1);
+    }
+    return findFloor(arr, val, index+1, ceiling);
+
   
 }
 
-//module.exports = findFloor
+module.exports = findFloor
